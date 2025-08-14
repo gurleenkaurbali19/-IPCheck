f843a040b79b50b21643a5bbc37d60c7b6e0b454
gurleenkaur19 <gurleenkaurbali20@gmail.com>
2025-08-14 13:26:35 +0530

    fix(cors): corrected allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware  # ← import CORS
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 app = FastAPI()
@@ -7,17 +7,15 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",              # Local development frontend
-        "https://ipcheck-nine.vercel.app/"    # deployed frontend URL
+        "http://localhost:3000",          # Local dev frontend
+        "https://ipcheck-nine.vercel.app" # Deployed frontend URL
     ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],   # All HTTP methods
+    allow_headers=["*"],   # All headers
 )
-
 
 app.include_router(router)
 
-
-#uvicorn app.main:app --reload
-#uvicorn app.main:app --host 0.0.0.0 --port $PORT
+# uvicorn app.main:app --reload
+# uvicorn app.main:app --host 0.0.0.0 --port $PORT
